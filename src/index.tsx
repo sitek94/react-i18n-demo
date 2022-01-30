5ffd534754f799b0ca9cceae9fef89b1cce0f5cf
Maciek Sitkowski <58401630+sitek94@users.noreply.github.com>
2022-01-30 19:54:26 +0100

    use i18next-http-backend to load translations as needed (#1)
    Also, use `React.Suspense` to show a spinner when loading translations.

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import './index.css'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
+import { OverlaySpinner } from 'components/spinner'
+
 import { App } from './app'
 import { initI18n } from './i18n'
 
 initI18n()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Suspense fallback={<OverlaySpinner />}>
+    <App /> ,
+  </React.Suspense>,
   document.getElementById('root')
 )
