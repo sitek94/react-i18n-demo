602160721c1e47667cc62e223c107f74027db63b
Maciek <msitkowski94@gmail.com>
2022-02-01 20:15:51 +0100

    hola, ¿qué tal? - add Español 💃🏽

--- src/components/language-switcher.tsx
+++ src/components/language-switcher.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 const languages = {
   en: { nativeName: '🇬🇧 English' },
   pl: { nativeName: '🇵🇱 Polski' },
+  es: { nativeName: '🇪🇸 Español' },
 }
 
 export function LanguageSwitcher() {
@@ -11,16 +12,22 @@ export function LanguageSwitcher() {
 
   return (
     <div className="language-switcher">
-      {Object.entries(languages).map(([language, { nativeName }]) => (
-        <button
-          type="button"
-          key={language}
-          disabled={language === i18n.resolvedLanguage}
-          onClick={() => i18n.changeLanguage(language)}
-        >
-          {nativeName}
-        </button>
-      ))}
+      {Object.entries(languages).map(([language, { nativeName }]) => {
+        const isSelected = language === i18n.resolvedLanguage
+        console.log({ language, resolved: i18n.resolvedLanguage })
+        return (
+          <button
+            type="button"
+            key={language}
+            aria-selected={isSelected}
+            disabled={isSelected}
+            className={isSelected ? 'selected' : ''}
+            onClick={() => i18n.changeLanguage(language)}
+          >
+            {nativeName}
+          </button>
+        )
+      })}
     </div>
   )
 }
